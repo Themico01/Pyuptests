4c8dea4ca9e54513bc3cffa3d6aebc3b31986086
Carlos Micó <cmicoreche@gmail.com>
2021-07-01 18:25:49 +0200

    Create dummy application to try updates

---
+++ Interface.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+from Mainttest import testupdate
+from pyupservice.client_config import ClientConfig
+
+
+
+version="0.0.1"
+CLIENT_CONFIG=ClientConfig()
+
+class Application:
+   
+    def __init__(self,window):
+            
+            self.wind=window
+            
+            login=LabelFrame(self.wind, text = 'Login')
+            login.grid(row = 0, column = 0, columnspan = 2)
+            
+            Label(login,text='Version: ').grid(row=0,column=0)
+            Label(login,text=version).grid(row=0,column=1)
+            
+            
+            
+if __name__=='__main__':
+    testupdate(CLIENT_CONFIG,version)
+    window=Tk()
+    application=Application(window)
+    window.mainloop() 
+
+
--- Mainttest.py
+++ Mainttest.py
@@ -1,63 +1,86 @@
 
+import sys
+import time
 
-
-from pyupservice.client_config import ClientConfig
+from tkinter import *
+from tkinter import messagebox
+from pyupdater.client import Client
 from pyupservice.fileserver import WaitForFileServerToStart
 
 
-CLIENT_CONFIG=ClientConfig()
 
-def CheckForUpdates(fileServerPort, debug):
+class UpdateStatus():
+    """Enumerated data type"""
+    UNKNOWN = 0
+    NO_AVAILABLE_UPDATES = 1
+    UPDATE_DOWNLOAD_FAILED = 2
+    EXTRACTING_UPDATE_AND_RESTARTING = 3
+    UPDATE_AVAILABLE_BUT_APP_NOT_FROZEN = 4
+    COULDNT_CHECK_FOR_UPDATES = 5
+
+UPDATE_STATUS_STR = \
+    ['Unknown', 'No available updates were found.',
+     'Update download failed.', 'Extracting update and restarting.',
+     'Update available but application is not frozen.',
+     'Couldn\'t check for updates.']
+
+"Función que comprueba so hay actualizaciones, la descarga y la instala"
+def CheckForUpdates(CLIENT_CONFIG,version):
     """
     Check for updates.
     Channel options are stable, beta & alpha
     Patches are only created & applied on the stable channel
     """
-    assert CLIENT_CONFIG.PUBLIC_KEY is not None
+    "Inicializa un cliente"
     client = Client (CLIENT_CONFIG, refresh=True)
-    appUpdate = client.update_check(CLIENT_CONFIG.APP_NAME,
-                                    wxupdatedemo.__version__,
-                                    channel='stable')
+    
+    "Comprueba si hay alguna actualización"
+    appUpdate = client.update_check(CLIENT_CONFIG.APP_NAME,version,channel='stable')
+    
     if appUpdate:
+        "Comprueba si el script forma parte de un bundle o es independiente"
         if hasattr(sys, "frozen"):
+            "Descarga el update"
             downloaded = appUpdate.download()
             if downloaded:
                 status = UpdateStatus.EXTRACTING_UPDATE_AND_RESTARTING
-                ShutDownFileServer(fileServerPort)
-                if debug:
-                    logger.debug('Extracting update and restarting...')
-                    time.sleep(10)
+                statusmessage=UPDATE_STATUS_STR[status]
+                messagebox.showinfo("Status",statusmessage)
                 appUpdate.extract_restart()
             else:
                 status = UpdateStatus.UPDATE_DOWNLOAD_FAILED
         else:
             status = UpdateStatus.UPDATE_AVAILABLE_BUT_APP_NOT_FROZEN
     else:
-        status = UpdateStatus.NO_AVAILABLE_UPDATES
-    return status
+        status = 0
+    if status is not 0:
+        statusmessage=UPDATE_STATUS_STR[status]
+        messagebox.showinfo("Status",statusmessage)
 
 
-
-
-
-def testupdate():
+def testupdate(CLIENT_CONFIG,version):
     
     "Carga la configuración de cliente"
-    print(CLIENT_CONFIG.APP_NAME)
-    print(CLIENT_CONFIG.UPDATE_URLS[0])
     url=CLIENT_CONFIG.UPDATE_URLS[0]
     timeout=CLIENT_CONFIG.HTTP_TIMEOUT
     
     "Comprobar si hay conexión con un servidor"
     connectionstatus=WaitForFileServerToStart(url,timeout)
-    if connectionstatus != None:
-        print(connectionstatus)
-    else:
-        print('No ha habido éxito')
-        
     
-    
-    
+    if connectionstatus:
+        if connectionstatus == 404:
+            status=UpdateStatus.COULDNT_CHECK_FOR_UPDATES
+            statusmessage=UPDATE_STATUS_STR[status]
+            messagebox.showinfo("Status",statusmessage)        
+        else:
+            status=CheckForUpdates(CLIENT_CONFIG,version)
 
-if __name__ == "__main__": 
-    testupdate()
+    else: 
+        status=UpdateStatus.COULDNT_CHECK_FOR_UPDATES
+        statusmessage=UPDATE_STATUS_STR[status]
+        messagebox.showinfo("Status",statusmessage)  
+    
+    
+      
+        
+       
